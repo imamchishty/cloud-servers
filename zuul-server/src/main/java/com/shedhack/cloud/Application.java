c00e250d1774050dda614d2c4fb2a4034e53bea3
Imam Chishty <imam.chishty@emc.com>
2016-10-04 17:06:09 +0400

    Zulu added into the mix.

--- zuul-server/src/main/java/com/shedhack/cloud/Application.java
+++ zuul-server/src/main/java/com/shedhack/cloud/Application.java
@@ -2,9 +2,11 @@ package com.shedhack.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class Application {
